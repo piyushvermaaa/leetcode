672ba9701b65b7f34c3cd515b00a44eca7addadf
Piyush Verma <vpiyush2278@gmail.com>
2026-06-25 00:15:36 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.86%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        import math
+        a = int(pow(x,0.5))
+        return a
