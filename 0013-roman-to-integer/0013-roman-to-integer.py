c57ddc88404af4a5136fac67882b038ad8097024
Piyush Verma <vpiyush2278@gmail.com>
2026-06-24 06:47:35 +0530

    Time: 16 ms (19.06%), Space: 12.5 MB (20.93%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def romanToInt(self, s):
+        n = len(s)
+        result = 0
+        letters = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+        for i in range(0,n):
+            if i < n-1 and letters[s[i]] < letters[s[i+1]]:
+                result -= letters[s[i]]
+            else:
+                result += letters[s[i]]
+        return result
+        
