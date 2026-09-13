0081d4685f815d4b2c40dafc52ca51eda2500ece
Piyush Verma <vpiyush2278@gmail.com>
2026-09-13 16:07:29 +0530

    Time: 10 ms (26.51%), Space: 47.5 MB (7.11%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
+        for(int i = 0; i < nums.length - 1; i+=2){
+            if(nums[i] != nums[i+1]){
                 return nums[i];
             }
         }
-
-        return nums[nums.length - 1];
+        return nums[nums.length-1];
     }
 }
